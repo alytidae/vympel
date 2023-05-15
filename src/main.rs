6483d24316151caac6ddc70bd5b28c5b5f0e4660
Igor Botukhov <igor.botukhov@gmail.com>
2023-05-16 01:21:43 +0200

    Initial architecture and show_help() method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+enum Command {
+    Show,
+    Add,
+    Rm,
+    All,
+}
+
+enum Arguments {
+    Message(String, String),
+    Number(u32),
+}
+struct Config {
+    command: Command,
+    arguments: Arguments,
+}
+
+fn show_help() {
+    println!("There are several commands that you can use:\n");
+    println!("tasks add \"Buy a milk\"         - add task \"Buy a milk\"");
+    println!("tasks add \"Watch a film\" high  - add a priority task (low, mid:default, high)");
+    println!("tasks rm 2                     - delete task with number 2");
+    println!("tasks all                      - show completed and incomplete tasks");
+}
+
+fn main() {    
 }
