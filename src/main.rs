d9681206c4e32d3e85b7dc46c652a82b59cb9a48
Igor Botukhov <igor.botukhov@gmail.com>
2023-05-17 00:11:16 +0200

    changing in Config::build method

--- src/main.rs
+++ src/main.rs
@@ -14,30 +14,32 @@ impl Command {
     fn build(args: &[String]) -> Result<Command, &'static str> {
         if args.len() == 1 {
             return Ok(Command::Show);
-        }else if args.len() == 2 && args[1] == "all" {
-            return Ok(Command::All);
-        }else if args.len() == 2 && args[1] == "help" {
-            return Ok(Command::Help);
-        }else if args.len() >= 2{
+        }else if args.len() == 2{
+            if args[1] == "help"{
+                return Ok(Command::Help);
+            }else if args[1] == "all" {
+                return Ok(Command::All);
+            }else{
+                return Err("Inaccurate command. For information on available commands, use:\n\ttasks help");
+            }
+        }else{
             todo!();
-        }else {
-            Err("Inaccurate command. For information on available commands, use:\n tasks help") 
         }
     }
 }
 
-fn show_help() {
-    println!("There are several commands that you can use:\n");
-    println!("tasks                          - show all tasks");
-    println!("tasks add \"Buy a milk\"         - add task \"Buy a milk\"");
-    println!("tasks add \"Watch a film\" high  - add a priority task (low, mid:default, high)");
-    println!("tasks rm 2                     - delete task with number 2");
-    println!("tasks all                      - show completed and incomplete tasks");
+fn command_help() {
+    println!("There are several commands that you can use:");
+    println!("\ttasks                       - show all tasks");
+    println!("\ttasks add \"Watch a film\"    - add task \"Buy a milk\"");
+    println!("\ttasks add \"Buy a milk\" high - add a priority task (low, mid:default, high)");
+    println!("\ttasks rm 2                  - delete task with number 2");
+    println!("\ttasks all                   - show completed and incomplete tasks");
 }
 
 fn execute_command(command: &Command) {
     match command {
-        Command::Help => show_help(),
+        Command::Help => command_help(),
         _ => (),
     }
 }
