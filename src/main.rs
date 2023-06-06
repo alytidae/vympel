aab5e7e0705699074d9278b26a6ba609c7076281
Igor Botukhov <igor.botukhov@gmail.com>
2023-06-06 22:46:30 +0200

    Add config build method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs;
 use std::process;
+use directories::ProjectDirs;
+use serde::Deserialize;
 
 enum Command {
     Help,
@@ -15,6 +17,10 @@ enum TaskPriority {
     Low,
     Mid,
     High,
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
 }
 
 impl Command {
@@ -88,15 +94,51 @@ impl Command {
     }
 }
 
+impl Config {
+    fn build() -> Result<Config, String> {
+        if let Some(proj_dirs) = ProjectDirs::from(
+            "dev",
+            "vympel",
+            "vympel",
+        ) {
+            let config_dir = proj_dirs.config_dir();
+            
+            let config_file = fs::read_to_string(
+                config_dir.join("config.toml")
+            );
+
+            let config: Config = match config_file {
+                Ok(file) => {
+                    match toml::from_str(&file) {
+                        Ok(toml_deserialize) => toml_deserialize,
+                        Err(err) => return Err(format!("Error in config: {}", err.message().to_string())),
+                    }
+                },
+                Err(_) => Config {
+                }
+            };
+            
+            return Ok(config);
+        }
+        Err(String::from("No valid home directory path"))
+    }
+}
+
 fn main() {    
+    let config = Config::build().unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
+    
     let args: Vec<String> = env::args().collect();
     let command = Command::build(&args).unwrap_or_else(|err| {
         println!("{err}");
         process::exit(1);
     });
     
-    match command.execute() {
-        Ok(_) => (),
-        Err(error) => println!("{error}"),
-    };
+   // match command.execute() {
+   //     Ok(_) => (),
+   //     Err(error) => println!("{error}"),
+   // };
+
 }
