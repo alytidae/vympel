87f905d600d122d7b9223cf763ab1566924ddaba
Igor Botukhov <igor.botukhov@gmail.com>
2023-06-07 17:48:42 +0200

    Delete comments and rewrite execute method

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::env;
 use std::fs;
 use std::process;
 use directories::ProjectDirs;
 use serde::Deserialize;
-use clap::{Args, Parser, Subcommand, ValueEnum};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
@@ -44,49 +43,6 @@ struct Config {
 }
 
 impl Command {
-    //fn build(args: &[String]) -> Result<Command, &'static str> {
-        //if args.len() == 1 {
-        //    return Ok(Command::Show);
-        //}else if args.len() == 2 {
-        //    if args[1] == "help"{
-        //        return Ok(Command::Help);
-        //    }else if args[1] == "all" {
-        //        return Ok(Command::All);
-        //    }else{
-        //        return Err("Inaccurate command. For information on available commands, use:\n\ttasks help");
-        //    }
-        //}else if args.len() == 3 && args[1] == "rm"{
-        //    match args[2].parse::<u32>(){
-        //        Ok(n) => return Ok(Command::Rm(n)),
-        //        Err(_) => return Err("Number of task must be integer. Like this:\n\ttasks rm 2     - remove task number 2"),
-        //    }
-        //}else if args.len() == 3 && args[1] == "add" {
-        //    Ok(Command::Add(args[2].clone(), TaskPriority::Mid))
-        //}else if args.len() == 4 && args[1] == "add" {
-        //    if args[3] == "low" {
-        //        Ok(Command::Add(args[2].clone(), TaskPriority::Low))
-        //    }else if args[3] == "mid" {
-        //        Ok(Command::Add(args[2].clone(), TaskPriority::Mid))
-        //    }else if args[3] == "high" {
-        //        Ok(Command::Add(args[2].clone(), TaskPriority::High))
-        //    }else{
-        //        Err("The priority of the task should be one of 3 options:\n\tlow\n\tmid\n\thigh")
-        //    }
-        //}else{
-        //    return Err("Inaccurate command. For information on available commands, use:\n\ttasks help");
-        //}
-    //}
-
-    //fn help(&self) -> Result<(), &'static str> {
-    //    println!("There are several commands that you can use:");
-    //    println!("\tvympel                       - show all tasks");
-    //    println!("\tvympel add \"Watch a film\"    - add task \"Buy a milk\"");
-    //    println!("\tvympel add \"Buy a milk\" high - add a priority task (low, mid:default, high)");
-    //    println!("\tvympel rm 2                  - remove task number 2");
-    //    println!("\tvympel all                   - show completed and incomplete tasks");
-    //    Ok(())
-    //}
-    
     fn show(&self, config: &Config) -> Result<(), &'static str>{
         todo!();
     }
@@ -103,15 +59,14 @@ impl Command {
         todo!();
     }
 
-    //fn execute(&self, config: &Config) -> Result<(), &'static str>{
-    //    match self {
-    //        Command::Help => self.help(),
-    //        Command::Show => self.show(config),
-    //        Command::Add(_,_) => self.add(config),
-    //        Command::Rm(_) => self.rm(config),
-    //        Command::All => self.all(config),
-    //    }
-    //}
+    fn execute(&self, config: &Config) -> Result<(), &'static str>{
+        match self {
+            Command::Show => self.show(config),
+            Command::Add { .. } => self.add(config),
+            Command::Rm { .. } => self.rm(config),
+            Command::All => self.all(config),
+        }
+    }
 }
 
 impl Config {
@@ -153,12 +108,7 @@ fn main() {
     let args = AppArgs::parse();
     let command = args.command.unwrap_or(Command::Show);
     dbg!(command);
-    //let args: Vec<String> = env::args().collect();
-    //let command = Command::build(&args).unwrap_or_else(|err| {
-    //    println!("{err}");
-    //    process::exit(1);
-    //});
-    
+
     //match command.execute(&config) {
     //    Ok(_) => (),
     //    Err(error) => println!("{error}"),
