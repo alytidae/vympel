4f1e5dea9b4f3f5e5fe1e2c2b4a724b0c76b79e3
Igor Botukhov <igor.botukhov@gmail.com>
2023-07-01 18:46:25 +0200

    Now we have semi-working Task::build()

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::{self, ReadDir};
 use std::io::{self, Write};
 use std::path::PathBuf;
@@ -11,7 +12,7 @@ pub struct Task {
     status: TaskStatus,
     creation_date: String,
     complete_date: Option<String>,
-    description: String,
+    description: Option<String>,
 }
 
 pub enum TaskStatus {
@@ -44,21 +45,54 @@ impl Task {
     fn build(path: PathBuf) -> Result<Task, String> {
         let data = fs::read_to_string(path).unwrap();
         let parts = data.split("---\n").collect::<Vec<&str>>();
-        let task = Task {
-            name: String::from("123"),
-            status: TaskStatus::Active,
-            creation_date: String::from("123"),
-            complete_date: Some(String::from("123")),
-            description: String::from("123"),
-        };
-        // We have something before metadata, then there is metadata after them there is a description
-        //if parts.len() == 3 {
-        //    for meta_field in parts[1].lines(){
-        //        println!("{}", meta_field);
-        //    }
-        //}
 
-        Ok(task)
+        let mut metadata: HashMap<String, String> = HashMap::new();
+
+        // We have something before metadata, then there is metadata after them there is a description
+        if parts.len() == 3 {
+            for meta_field in parts[1].lines() {
+                let pair = meta_field.splitn(2, ":").collect::<Vec<&str>>();
+                let meta_key = pair[0];
+                let meta_value = pair[1].replace("\"", "");
+                metadata.insert(meta_key.to_string(), meta_value);
+            }
+            
+            //TODO: rewrite, its awful
+            let task_name = metadata.get("name").unwrap().trim().to_string();
+            let task_status;
+            if metadata.get("status").unwrap().trim().to_string() == "active" {
+                task_status = TaskStatus::Active;
+            }else if metadata.get("status").unwrap().trim().to_string() == "inactive" {
+                task_status = TaskStatus::Inactive;
+            }else {
+                return Err(String::from("Cant parse status field in task"));
+            }
+            let task_creation_date = metadata.get("creation_date").unwrap().trim().to_string();
+            let task_complete_date;
+            if metadata.get("complete_date").unwrap().trim().to_string() == "" {
+                task_complete_date = None;
+            }else {
+                task_complete_date = Some(metadata.get("complete_date").unwrap().trim().to_string());
+            }
+            let task_description;
+            if parts[2] == "" {
+                task_description = None;
+            }else {
+                task_description = Some(parts[2].trim().to_string());
+            }
+
+            let task = Task {
+                name: task_name,
+                status: task_status,
+                creation_date: task_creation_date,
+                complete_date: task_complete_date,
+                description: task_description,
+            };
+            
+            return Ok(task)
+        }
+
+        Err(String::from("Failed to parse task"))
     }
 
     pub fn print_short(&self) {
